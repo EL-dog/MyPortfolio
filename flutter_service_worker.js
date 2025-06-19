4a4d5994eff11d1623bb7d91b612c4609ba70901
Saaaahillll <ojhasahil9@gmail.com>
2025-06-19 17:21:13 +0530

    Ui changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3c8c76efcc21ed3ff4b18ff4e00789a1",
+const RESOURCES = {"flutter_bootstrap.js": "87503733527d01c563a2412997be4d46",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -12,6 +12,24 @@ const RESOURCES = {"flutter_bootstrap.js": "3c8c76efcc21ed3ff4b18ff4e00789a1",
 "index.html": "971bd1122936506d4aa9634521bafabb",
 "/": "971bd1122936506d4aa9634521bafabb",
 "manifest.json": "1f9c93ad7cc560cfca1e619b3576bbe0",
+".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58",
+".git/hooks/fsmonitor-watchman.sample": "a0b2633a2c8e97501610bd3f73da66fc",
+".git/hooks/pre-commit.sample": "305eadbbcd6f6d2567e033ad12aabbc4",
+".git/hooks/push-to-checkout.sample": "c7ab00c7784efeadad3ae9b228d4b4db",
+".git/hooks/pre-merge-commit.sample": "39cb268e2a85d436b9eb6f47614c3cbc",
+".git/hooks/post-update.sample": "2b7ea5cee3c49ff53d41e00785eb974c",
+".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
+".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
+".git/hooks/update.sample": "647ae13c682f7827c22f5fc08a03674e",
+".git/hooks/sendemail-validate.sample": "4d67df3a8d5c98cb8565c07e42be0b04",
+".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
+".git/hooks/pre-push.sample": "2c642152299a94e05ea26eae11993b13",
+".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
+".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
+".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
+".git/config": "5b603c2c0801a9ded3b79159fc38b404",
+".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
+".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "canvaskit/canvaskit.js.symbols": "68eb703b9a609baef8ee0e413b442f33",
@@ -38,7 +56,7 @@ const RESOURCES = {"flutter_bootstrap.js": "3c8c76efcc21ed3ff4b18ff4e00789a1",
 "assets/fonts/MaterialIcons-Regular.otf": "0f75acdda9e0afda19a35f123dd9c8cd",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "version.json": "58250a58ccef6ff01542b0630c61cdcb",
-"main.dart.js": "e3ecbf35c55fe3ed05d38ee34eb4036d"};
+"main.dart.js": "06dd6224b9c1a14afa88dd4008904885"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
